ded8c58ad539b36e2bafb8ffb85490e2c89a3ef2
Matthew Mills <Matthew.Mills@investec.co.za>
2016-12-09 16:22:41 +0200

    more kata's and WIP find-the-smallest

---
+++ replaceAlphabetWithAlphabetPosition.js
@@ -0,0 +1,13 @@
+//Replace alphabet with alphabet position
+
+function alphabetPosition(text) {
+  var alpha = "abcdefghijklmnopqrstuvwxyz";
+  var returnStr = "";
+  for (i = 0; i < text.length; i++){
+    var x = alpha.indexOf(text[i].toLowerCase());
+    if (x != -1){
+      returnStr += (x+1) + " ";
+    }
+  }
+  return returnStr.substr(0, returnStr.lastIndexOf(" "));
+}
